aad78cd09555d2fcbb329f6f2ac7421e89bc255a
Stephen McCray <smc@rentrak.com>
2016-01-18 12:31:41 -0800

    Style fixes for type_tag

--- python/Payer/payer/type_tag.py
+++ python/Payer/payer/type_tag.py
@@ -1,6 +1,15 @@
 class TypeTag(object):
-    def __init__(self, name): self.name = name;
-    def __str__(self): return self.name;
-    def __repr__(self): return self.name;
-    def __call__(self, *args): return (self,) + tuple(args);
+    def __init__(self, name):
+        self.name = name
 
+
+    def __str__(self):
+        return self.name
+
+
+    def __repr__(self):
+        return self.name
+
+
+    def __call__(self, *args):
+        return (self,) + tuple(args)
